22ecfe931f9e0429441977c25cacb415efd3d79a
TheLexG <agentezayn007@gmail.com>
2024-08-10 12:33:45 -0600

    Login_Registro

---
+++ src/controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../src/models/UserModel.php';
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $npesonal = $_POST['npesonal'];
+    $password = $_POST['password'];
+
+    $userModel = new UserModel($pdo);
+    $user = $userModel->authenticate($npesonal, $password);
+
+    if ($user) {
+        $_SESSION['user'] = $user;
+        header('Location: index.php');
+        exit;
+    } else {
+        $error = 'Credenciales incorrectas';
+    }
+}
+?>
